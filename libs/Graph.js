8327960f69bd4732cae49e72a3d36492893a0d41
Alberto Restifo <alberto.restifo@gmail.com>
2015-08-27 08:31:25 +0200

    :memo: Improve code documentation, add cost option
    - Improve the code inline documentation
    - Add option to return an object containing the total path cost

--- libs/Graph.js
+++ libs/Graph.js
@@ -6,9 +6,9 @@ const populateMap = require('./populateMap')
 class Graph {
 
   /**
-   * Construct the base graph
+   * Constrict the graph
    *
-   * @param {object} [graph] - Initialize the graph with nodes
+   * @param {object} [graph] - Nodes to initiate the graph with
    */
   constructor (graph) {
     this.graph = new Map()
@@ -17,10 +17,10 @@ class Graph {
   }
 
   /**
-   * Add a vertex to the vertices map
+   * Add a node to the graph
    *
-   * @param {string} name      - Name of the vertex
-   * @param {object} neighbors - Neighbouring nodes
+   * @param {string} name      - Name of the node
+   * @param {object} neighbors - Neighbouring nodes and cost to reach them
    */
   addVertex (name, neighbors) {
     let _neighbors = new Map()
@@ -32,36 +32,50 @@ class Graph {
   }
 
   /**
-   * Compute the shortest path between the specified vertices
+   * Compute the shortest path between the specified nodes
    *
-   * @param {string}  start        - Origin node
-   * @param {string}  goal         - Destination node
-   * @param {object}  [options]    - Options
+   * @param {string}  start     - Starting node
+   * @param {string}  goal      - Node we want to reach
+   * @param {object}  [options] - Options
    *
-   * @param {boolean} [options.trim]    - Exclude the origin and destination vertices from the result
+   * @param {boolean} [options.trim]    - Exclude the origin and destination nodes from the result
    * @param {boolean} [options.reverse] - Return the path in reversed order
+   * @param {boolean} [options.cost]    - Also return the cost of the path when set to true
    *
-   * @return {array} Computed path between the nodes
+   * @return {array|object} Computed path between the nodes.
+   *  When `option.cost` is set to true, the returned value will be an object
+   *  with keys:
+   *
+   *    - `Array path`: Computed path between the nodes
+   *    - `Number cost`: Cost of the path
    */
   path (start, goal, options) {
     options = options || {}
 
-    // If we have no vertices set, we return null
+    // Don't run when we don't have nodes set
     if (!this.graph.size) return null
 
-    // Use Uniform-cost Search for better performances
-    let frontier = new Queue()
     let explored = new Set()
+    let frontier = new Queue()
     let previous = new Map()
-    let path = []
 
+    let path = []
+    let totalCost = 0
+
+    // Add the starting point to the frontier, it will be the first node visited
     frontier.set(start, 0)
 
+    // Run until we have visited every node in the frontier
     while (!frontier.isEmpty()) {
+      // Get the node in the frontier with the lowest cost (`priority`)
       let node = frontier.next()
 
+      // When the node with the lowest cost in the frontier in our goal node,
+      // we can compute the path and exit the loop
       if (node.key === goal) {
-        // RESULT
+        // Set the total cost to the current value
+        totalCost = node.priority
+
         let _nodeKey = node.key
         while (previous.has(_nodeKey)) {
           path.push(_nodeKey)
@@ -71,13 +85,17 @@ class Graph {
         break
       }
 
+      // Add the current node to the explored set
       explored.add(node.key)
 
+      // Loop all the neighboring nodes
       let neighbors = this.graph.get(node.key) || new Map()
       neighbors.forEach(function (_cost, _node) {
-        // Skip the neighbor if it was already explored
+        // If we already explored the node, skip it
         if (explored.has(_node)) return false
 
+        // If the neighboring node is not yet in the frontier, we add it with
+        // the correct cost
         if (!frontier.has(_node)) {
           previous.set(_node, node.key)
           return frontier.set(_node, node.priority + _cost)
@@ -86,6 +104,8 @@ class Graph {
         var frontierPriority = frontier.get(_node).priority
         var nodeCost = node.priority + _cost
 
+        // Othewhise we only update the cost of this node in the frontier when
+        // it's below what's currently set
         if (nodeCost < frontierPriority) {
           previous.set(_node, node.key)
           frontier.set(_node, nodeCost)
@@ -94,23 +114,35 @@ class Graph {
     }
 
     // Return null when no path can be found
-    if (!path.length) return null
+    if (!path.length) {
+      if (options.cost) return { path: null, cost: 0 }
+
+      return null
+    }
 
     // From now on, keep in mind that `path` is populated in reverse order,
     // from destination to origin
 
-    // Remove the last value (that will be origin) if we want the result to
-    // be trimmed
+    // Remove the first value (the goal node) if we want a trimmed result
     if (options.trim) {
       path.shift()
     } else {
-      // Add the origin waypoint at the beginning of the array
+      // Add the origin waypoint at the end of the array
       path = path.concat([ start ])
     }
 
-    // Reverse the path if we don't want it reversed
+    // Reverse the path if we don't want it reversed, so the result will be
+    // from `start` to `goal`
     if (!options.reverse) {
       path = path.reverse()
+    }
+
+    // Return an object if we also want the cost
+    if (options.cost) {
+      return {
+        path: path,
+        cost: totalCost
+      }
     }
 
     return path
