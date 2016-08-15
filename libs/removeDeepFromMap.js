947193dfd1c435cd1f27fe3f34638058801bd4ae
Alberto Restifo <alberto.restifo@gmail.com>
2016-08-15 17:36:36 +0200

    Implement node remove

---
+++ libs/removeDeepFromMap.js
@@ -0,0 +1,25 @@
+/**
+ * Removes a key and all it's referencies form a map. This function oeprates withous side-effects
+ * and returns a new map.
+ *
+ * @param {Map}     map - Map to remove the key from
+ * @param {string}  key - Key to remove from the map
+ * @return {Map} New map without the passed key
+ */
+function removeDeepFromMap(map, key) {
+  const newMap = new Map();
+
+  for (const aKey of map) {
+    const val = map.get(aKey);
+
+    if (aKey !== key && val instanceof Map) {
+      newMap.set(aKey, removeDeepFromMap(val, key));
+    } else if (aKey !== key) {
+      newMap.set(aKey, val);
+    }
+  }
+
+  return newMap;
+}
+
+module.exports = removeDeepFromMap;
