01330de43492d8cc67c3b8fe843c7aac1870aa40
Nate Dobbins <nate.dobbins@expressimage.com>
2017-05-03 11:12:48 -0500

    Add tests for avoid

--- test/Graph.test.js
+++ test/Graph.test.js
@@ -214,6 +214,66 @@ describe('Graph', () => {
       res.cost.must.equal(0);
     });
 
+    it('returns the same path regardless of whether a node which is not part of the calculated shortest path is avoided',
+    () => {
+      const route = new Graph({
+        a: { b: 1 },
+        b: { a: 1, c: 1 },
+        c: { b: 1, d: 1 },
+        d: { c: 1 },
+      });
+
+      const path = route.path('a', 'c', { cost: true });
+      const path2 = route.path('a', 'c', { avoid: ['d'], cost: true });
+
+      path.must.eql(path2);
+    });
+
+    it('returns a different path if a node which is part of the calculated shortest path is avoided',
+    () => {
+      const route = new Graph({
+        a: { b: 1, c: 50 },
+        b: { a: 1, c: 1 },
+        c: { a: 50, b: 1, d: 1 },
+        d: { c: 1 },
+      });
+
+      const res = route.path('a', 'd', { cost: true });
+      const res2 = route.path('a', 'd', { avoid: ['b'], cost: true });
+
+      res.path.must.not.eql(res.path2);
+      res.cost.must.be.at.most(res2.cost);
+    });
+
+    it('throws an error if the start node is avoided',
+    () => {
+      const route = new Graph(vertices);
+
+      const res = route.path('a', 'c', { avoid: ['a'] });
+
+      res.must.be.an.error(Error);
+    });
+
+    it('throws an error if the end node is avoided',
+    () => {
+      const route = new Graph(vertices);
+
+      const res = route.path('a', 'c', { avoid: ['c'] });
+
+      res.must.be.an.error(Error);
+    });
+
+    it('returns the same path and cost if a node which is not part of the graph is avoided',
+    () => {
+      const route = new Graph(vertices);
+
+      const res = route.path('a', 'c', { cost: true });
+      const res2 = route.path('a', 'c', { avoid: ['z'], cost: true });
+
+      res.path.must.eql(res.path2);
+      res.cost.must.equal(res2.cost);
+    });
+
     it('works with a more complicated graph', () => {
       const route = new Graph({
         a: { b: 7, c: 9, f: 14 },
